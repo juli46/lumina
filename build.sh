7eb9c0e594bb35627532bcceb667649d2de56dee
juli46 <hkitty2666@gmail.com>
2026-09-21 11:19:43 -0500

    Verificar archivos media en despliegue

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "=== COMPROBANDO MEDIA ==="
+pwd
+echo "--- archivo queen ---"
+ls -lh media/productos/queen.webp
+echo "--- carpeta media/productos ---"
+ls -lh media/productos | head -20
+echo "=== FIN COMPROBACION MEDIA ==="
+
 pip install -r requirements.txt
 
 python manage.py migrate
