1bc588ff14c14ee7fb053070928366375aa71920
juli46 <hkitty2666@gmail.com>
2026-09-21 11:39:22 -0500

    Corregir verificación de media

--- build.sh
+++ build.sh
@@ -8,8 +8,11 @@ ls -lh media/productos/queen.webp
 echo "--- carpeta media/productos ---"
 ls -lh media/productos | head -20
 echo "=== FIN COMPROBACION MEDIA ==="
+
+pip install -r requirements.txt
+
 echo "=== DJANGO MEDIA CONFIG ==="
-python -c "import os; os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings'); import django; django.setup(); from django.conf import settings; import os.path; print('MEDIA_ROOT =', settings.MEDIA_ROOT); print('EXISTS =', os.path.exists(settings.MEDIA_ROOT)); print('FILE EXISTS =', os.path.exists(os.path.join(settings.MEDIA_ROOT, 'productos', 'queen.webp')))"
+python -c "import os; os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings'); import django; django.setup(); from django.conf import settings; print('MEDIA_ROOT =', settings.MEDIA_ROOT); print('EXISTS =', os.path.exists(settings.MEDIA_ROOT)); print('FILE EXISTS =', os.path.exists(os.path.join(settings.MEDIA_ROOT, 'productos', 'queen.webp')))"
 echo "=== FIN DJANGO MEDIA CONFIG ==="
 
 python manage.py migrate
