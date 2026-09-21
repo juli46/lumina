8111f0241ff73b0024a52df8cd925c977f7b0cdb
juli46 <hkitty2666@gmail.com>
2026-09-21 11:43:54 -0500

    Servir archivos media en producción

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +9,13 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+# Servir archivos media también cuando DEBUG=False
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {
+            'document_root': settings.MEDIA_ROOT,
+        }
+    ),
+]
